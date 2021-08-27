70f84cb004f83ea95fb70a027b89bc2547f77918
2Hunter2 <hunter1griswold@gmail.com>
2021-08-27 14:56:18 -0700

    Account Class Setup
    Account Class instance fields and constructor

---
+++ src/Account.java
@@ -0,0 +1,11 @@
+
+public class Account {
+	private double balance;
+	private String bankID;
+	
+	public Account (double balance, String bankID)
+	{
+		this.balance = balance;
+		this.bankID = bankID;
+	}
+}
