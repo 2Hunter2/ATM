5a6eb2cf8be415ba3993184c5707f58866accb6c
2Hunter2 <hunter1griswold@gmail.com>
2021-08-31 13:47:42 -0700

    Tester

--- src/Tester.java
+++ src/Tester.java
@@ -4,21 +4,21 @@ public class Tester {
     public static void main(String[] args)
     {
   
-        // Creating an empty HashMap
+
         HashMap<String, Double> maps = new HashMap<String, Double>();
   
-        // Mapping int values to string keys
+
         maps.put("Geeks", 10.0);
         maps.deposit ("You", 10.0);
      
   
-        // Displaying the HashMap
+
         System.out.println("Initial Mappings are: " + maps);
   
-        // Getting the value of "Geeks"
+      
         System.out.println("The Value is: " + maps.get("Geeks"));
   
-        // Getting the value of "You"
+      
         System.out.println("The Value is: " + maps.get("You"));
     }
 }
