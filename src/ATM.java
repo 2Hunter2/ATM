0b8d9ad74cd292fa425c889cb2f35271d60991b1
benv123 <89551265+benv123@users.noreply.github.com>
2021-09-02 14:07:27 -0700

    Added Tester and withdraw(), checkBalance(), & toString() methods (#1)

--- src/ATM.java
+++ src/ATM.java
@@ -2,26 +2,45 @@
 import java.util.HashMap;
 
 public class ATM {
-	private HashMap <String, Double> maps = new HashMap <> ();
-	private String bankID;
-
-	public ATM (String bankID)
-	{
-		this.bankID = bankID;
-	}
-
-	private void deposit (String bankID, Double value)
-	{
-		if (value > 0)
-		{
-			if (maps.containsKey(this.bankID))
-			{
-				maps.put(this.bankID, maps.get (this.bankID) + value);
+	private HashMap<String,Double> maps=new HashMap<>();
+	public ATM(){}
+	public void deposit (String bankID, Double value) throws Exception{
+		if (value>0){
+			if (maps.containsKey(bankID)){
+				maps.put(bankID,maps.get(bankID)+value);
 			}
-			else
-			{
-				maps.put(this.bankID, value);
+			else{
+				maps.put(bankID,value);
 			}
 		}
+		else{
+			throw new Exception("You cannot deposit a negative sum of money!");
+		}
+	}
+	public void withdraw(String bankID,Double value) throws Exception{
+		if(value>0){
+			if(maps.containsKey(bankID)){
+				maps.put(bankID, maps.get(bankID)-value);
+			}
+			else{
+				System.out.println("An account with that bank ID does not exist!");
+			}
+		}
+		else{
+			throw new Exception("You cannot withdraw a negative sum of money!");
+		}
+	}
+	public String checkBalance(String bankID) throws Exception{
+		if(maps.containsKey(bankID)) {
+			return(""+maps.get(bankID));
+		}
+		throw new Exception("Bank account with that ID doesn't exist!");
+	}
+	public String toString(){
+		String str="";
+		for(HashMap.Entry<String,Double> account:maps.entrySet()){
+			str+=account.getKey()+": $"+account.getValue()+"\n";
+		}
+		return str;
 	}
 }
---
+++ src/ATMtester.java
@@ -0,0 +1,17 @@
+
+public class ATMtester{
+	public static void main(String[] args) throws Exception{
+		ATM atm=new ATM();
+		atm.deposit("Winfrey",25159684372.43);
+		atm.deposit("Bezos",193495330293.43);
+		atm.deposit("Bezos",20394353.35);
+		atm.withdraw("Bezos",3593293.66);
+		atm.deposit("MrTheiss",64033.55);
+		atm.withdraw("MrTheiss",443964.46);
+		atm.checkBalance("Bezos");
+		atm.checkBalance("Winfrey");
+		System.out.println(atm.toString());
+		atm.checkBalance("YoMama");
+		atm.checkBalance("MrTheiss");
+	}
+}
